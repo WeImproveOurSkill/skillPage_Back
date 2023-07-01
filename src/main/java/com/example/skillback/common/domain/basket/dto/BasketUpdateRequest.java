dde54c988de757a46bae7ff8c62d0f1826d74983
skyriv213 <gn1007@dankook.ac.kr>
2023-07-01 23:17:46 +0900

    <feat> <(basket)> : basket 연결을 위한 blank API 작성

---
+++ src/main/java/com/example/skillback/common/domain/basket/dto/BasketUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.example.skillback.common.domain.basket.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class BasketUpdateRequest {
+
+    private Long productCnt;
+}
