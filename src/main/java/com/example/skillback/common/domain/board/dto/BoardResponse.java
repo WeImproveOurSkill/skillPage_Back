3690f33203736d580329adb9954f98f6d948b201
skyriv213 <gn1007@dankook.ac.kr>
2023-08-07 22:06:22 +0900

    #30 <refact> <(Board)> : CommentResponse 추가 , BoaedResponse 사용 시 CommentResponse로 Comment 값 가져오기

--- src/main/java/com/example/skillback/common/domain/board/dto/BoardResponse.java
+++ src/main/java/com/example/skillback/common/domain/board/dto/BoardResponse.java
@@ -1,6 +1,9 @@
 package com.example.skillback.common.domain.board.dto;
 
 import com.example.skillback.common.domain.board.entity.Board;
+import com.example.skillback.common.domain.comment.dto.CommentResponse;
+import com.example.skillback.common.domain.comment.entity.Comment;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +18,12 @@ public class BoardResponse {
     private String title;
     private String content;
     private String userName;
+    private List<CommentResponse> comments;
 
-    public BoardResponse(Board board) {
-        this.boardId = board.getId();
-        this.title = board.getTitle();
-        this.content = board.getContent();
-        this.userName = board.getUser().getUserName();
-    }
+//    public BoardResponse(Board board) {
+//        this.boardId = board.getId();
+//        this.title = board.getTitle();
+//        this.content = board.getContent();
+//        this.userName = board.getUser().getUserName();
+//    }
 }
