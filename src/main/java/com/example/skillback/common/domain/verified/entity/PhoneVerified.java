2b153c398905a9af69a1092667b4e16b69ea7178
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2023-08-22 19:04:51 +0900

    Dev (#43)
    * #40 <fix> <(User)> :  filter, config 재작성, 회원가입시에도 바로 token발급하도록 변경
    
    * #40 <feat> <(verified)> : 휴대폰 인증을 위한 네이버 클라우드 연결
    
    * #40 <feat> <(Verified)> :  휴대폰 인증 부분 구현 - 테스트 x (발신번호 변작 방지 서비스로 인해 인터넷 문자 발신 x)
    
    * #42 <feat> <(Redis)> : 기본 환경 구성
    
    * #40 (remove) <(Verified)> : 오타 코드 rename 후 잔여 기록
    
    * #40 <fix> <(verified)> : 휴대폰 인증 문자 관련 수정 및 postman 테스트 완료

--- src/main/java/com/example/skillback/common/domain/verified/entity/PhoneVerified.java
+++ src/main/java/com/example/skillback/common/domain/verified/entity/PhoneVerified.java
@@ -6,25 +6,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
 
+
+//@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
-@Entity
-@Table(name = "phone_verified")
+@RedisHash(value = "phone_verified",timeToLive = 180)
 public class PhoneVerified {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String phoneNumber;
 
     private String code;
 
+    private LocalDateTime createdAt;
+
+    public PhoneVerified(String phoneNumber, String code) {
+        this.phoneNumber = phoneNumber;
+        this.code = code;
+        this.createdAt = LocalDateTime.now();
+    }
 }
